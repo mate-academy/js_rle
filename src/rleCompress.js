6fc555b6d223244a9360becb3117c9a217fe6c18
Andrey Faychak <faychak333@gmail.com>
2020-03-26 19:02:09 +0200

    'solution'

--- src/rleCompress.js
+++ src/rleCompress.js
@@ -18,7 +18,26 @@
  * @return {string}
  */
 function rleCompress(source) {
-  // write code here
+  let result = ""
+  for (let i = 0; i < source.length; i++) {
+    let count = 1;
+    result = result + source[i];
+    for (let j = i + 1; j < source.length; j++) {
+      if (source[i] == source[j] && j == source.length - 1) {
+        count++;
+        i = source.length;
+      } else if (source[i] == source[j]) {
+        count++;
+      } else {
+        i = j - 1;
+        j = source.length;
+      }
+    }
+    if (count >= 2) {
+      result = result + count;
+    }
+  }
+  return result;
 }
 
 module.exports = rleCompress;
