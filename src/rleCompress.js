4d590aca4fac6cfa3e936222cc5e25044a43800d
VlaDinter <vladyslavdinter@gmail.com>
2020-05-22 07:03:34 +0300

    Solution

--- src/rleCompress.js
+++ src/rleCompress.js
@@ -1,24 +1,26 @@
 'use strict';
 
-/**
- * Implement rleCompress function:
- *
- * Function takes string argument and returns compressed string.
- * Compression algorithm is the following: 2 and more identical
- * letters in a row are replaced with the letter and count of
- * repeating (don’t add number 1 for one letter)
- *
- * rle('A') === 'A'
- * rle('ABC') === 'ABC'
- * rle('AABDE') === 'A2BDE'
- * rle('ABGGGDKKKUKKF') === 'ABG3DK3UK2F'
- *
- * @param {string} source
- *
- * @return {string}
- */
 function rleCompress(source) {
-  // write code here
+  let compressedString = '';
+  let letterCount = 1;
+
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] !== source[i - 1]) {
+      if (letterCount > 1) {
+        compressedString += letterCount;
+        letterCount = 1;
+      }
+
+      compressedString += source[i];
+    } else if (i === source.length - 1) {
+      letterCount++;
+      compressedString += letterCount;
+    } else {
+      letterCount++;
+    }
+  }
+
+  return compressedString;
 }
 
 module.exports = rleCompress;
